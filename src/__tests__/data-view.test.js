da15d6432837506b377af4451b35f3d127ac76e7
Jose Collas <joseherminiocollas@goatstone.com>
2022-05-31 20:35:57 -0500

    add getAll, listen, set to DataView, test

--- src/__tests__/data-view.test.js
+++ src/__tests__/data-view.test.js
@@ -3,5 +3,22 @@ import DataView from 'data-view/data-view';
 describe('DataView', () => {
     it('should return and object', () => {
         expect(DataView([])).toBeTruthy();
+    });
+    it('should get values of dataViews', () => {
+        const expected = [{ id: 'x', name: 'x' }];
+        const dataView = DataView(expected);
+        const count = Object.values(dataView.getAll()).length;
+        expect(count).toBe(expected.length);
+    })
+    it('should set value and listen for that value', async () => {
+        const dV = DataView([{ id: 'x', name: 'x' }]);
+        const id = 'x';
+        dV.set({ id, name: 'x' });
+        const a = await new Promise((res, rej) => {
+            dV.listen(v => {
+                res(v.id)
+            });
+        });
+        expect(a).toBe(id);
     })
 });
--- src/data-view/data-view.ts
+++ src/data-view/data-view.ts
@@ -1,6 +1,22 @@
-const DataView = () => {
+import { BehaviorSubject } from 'rxjs';
+
+const DataView = (data: { id: string }[]) => {
+    const items: {[s: string]: any} = data.reduce((acc, v, i) => {
+        return { ...acc, [v.id]: v };
+    }, {});
+    const itemUpdate$ = new BehaviorSubject(null)
+    itemUpdate$.subscribe((v: { id: string } | null) => {
+        if (!v) return;
+        items[v.id] = v;
+    })
+    const getAll = () => items;
+    const set = (v: any) => itemUpdate$.next(v)
+    const listen = (cb: any) => itemUpdate$.subscribe( v => cb(v))
     return {
-        a: 1,
-    }
+        getAll,
+        listen,
+        set,
+    };
 }
+
 export default DataView;
