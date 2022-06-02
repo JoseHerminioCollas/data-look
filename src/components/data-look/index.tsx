6b997a27104475124967fbe4e13b3383db18573e
Jose Collas <joseherminiocollas@goatstone.com>
2022-06-02 13:18:05 -0500

    add details to display

--- src/components/data-look/index.tsx
+++ src/components/data-look/index.tsx
@@ -4,20 +4,39 @@ import { DataStyles } from 'data-style'
 type DataLookI = (props: { dataStyles: DataStyles }) => React.ReactElement
 
 const DataLook: DataLookI = ({ dataStyles }) => {
-    return (<div className='view-container'>
-        {
-            Object.entries(dataStyles).map(([k, v]) => (
-                <div
-                    key={k}
-                    style={{
-                        background: v.background
-                    }}
-                >
-                    {v.data.name}
-                </div>
-            ))
-        }
-    </div>)
+  const detailList = ['date_added', 'a', 'b', 'c', 'd']
+  return (<div className='view-container'>
+    {
+      Object.entries(dataStyles).map(([k, v]) => (
+        <div
+          key={k}
+          style={{
+            background: v.background
+          }}
+        >
+          {v.data.name}
+          {Object.entries(v.data)
+            .filter(([k, v]) => detailList.includes(k))
+            .map(([k, v]) => {
+              let dt = k
+                .split('_')
+                .map(l => `${l.charAt(0).toUpperCase()}${l.slice(1)}`)
+                .join(' ')
+              let dd = v
+              if (k === 'date_added') {
+                dd = new Date(String(v)).toDateString()
+              } else if (v === null) {
+                dd = ' - '
+              }
+              return (
+                <dl>
+                  <dt>{dt}</dt>&nbsp;:&nbsp;<dd>{dd}</dd>
+                </dl>)
+            })}
+        </div>
+      ))
+    }
+  </div>)
 }
 
 export default DataLook;
--- src/get-list.ts
+++ src/get-list.ts
@@ -1,7 +1,9 @@
 const getList = () => {
     return [...Array(100)].map((v, i) => ({
       id: String(i),
-      name: String.fromCodePoint(Math.floor(Math.random() * 10) + 65)
+      name: String.fromCodePoint(Math.floor(Math.random() * 10) + 65),
+      a: 'a',
+      b: 'b',
     }))
   }
   
