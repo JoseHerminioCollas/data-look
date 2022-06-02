4ecdd9a20648656af0b6ab02fd1131d92e9620ac
Jose Collas <joseherminiocollas@goatstone.com>
2022-06-02 11:48:53 -0500

    fix engine

--- src/engine.ts
+++ src/engine.ts
@@ -1,18 +1,17 @@
 const engine = (dV: any, rate: number) => {
-    const cI = setInterval(() => {
-      const n = (Math.floor((Math.random() * 15))).toString(16)
-      const n2 = (Math.floor((Math.random() * 15))).toString(16)
-      const n3 = (Math.floor((Math.random() * 15))).toString(16)
-      const color = `#${n}${n2}${n3}`
-      const ranDI = Math.floor(Math.random() * Object.values(dV.getAll()).length).toString()
-      const s = dV.getAll()[ranDI]
-      console.log('s', dV.getAll(), rate)
-      const s2 = { ...s, background: color }
-      dV.set(s2)
-    }, rate)
-    // setTimeout(() => clearInterval(cI), 5000)
-    return cI
-  }
+  const cI = setInterval(() => {
+    const n = (Math.floor((Math.random() * 15))).toString(16)
+    const n2 = (Math.floor((Math.random() * 15))).toString(16)
+    const n3 = (Math.floor((Math.random() * 15))).toString(16)
+    const color = `#${n}${n2}${n3}`
+    const allIds = Object.keys(dV.getAll())
+    const randAllId = Math.floor(Math.random() * allIds.length)
+    const s = dV.getAll()[allIds[randAllId]] //[randAllId] 
+    const s2 = { ...s, background: color }
+    dV.set(s2)
+  }, rate)
+  // setTimeout(() => clearInterval(cI), 25000)
+  return cI
+}
 
-  export default engine;
-  
+export default engine;
