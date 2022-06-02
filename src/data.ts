ca59cb808d23659e0dc3fa641a4df06a5b44dd1e
Jose Collas <joseherminiocollas@goatstone.com>
2022-06-02 11:48:34 -0500

    add data

---
+++ src/data.ts
@@ -0,0 +1,65 @@
+const data = {
+    "status": {
+        "timestamp": "2022-05-28T00:08:01.639Z",
+    },
+    "data": [
+        {
+            "id": 1,
+            "name": "Abc",
+            "symbol": "ABC",
+            "slug": "abc",
+            "date_added": "2013-04-28T00:00:00.000Z",
+            "tags": [
+                null
+            ],
+            "a": 21000000,
+            "b": 19051150,
+            "c": 19051150,
+            "d": null,
+        },
+        {
+            "id": 2,
+            "name": "Def",
+            "symbol": "DEF",
+            "slug": "def",
+            "date_added": "2013-04-28T00:00:00.000Z",
+            "tags": [
+                null
+            ],
+            "a": 21000000,
+            "b": 19051150,
+            "c": 19051150,
+            "d": null,
+        },
+        {
+            "id": 3,
+            "name": "Ghi",
+            "symbol": "GHI",
+            "slug": "ghi",
+            "date_added": "2013-01-28T00:00:00.000Z",
+            "tags": [
+                null
+            ],
+            "a": 21000000,
+            "b": 19051150,
+            "c": 19051150,
+            "d": null,
+        },
+        {
+            "id": 4,
+            "name": "Jkl",
+            "symbol": "JKL",
+            "slug": "jkl",
+            "date_added": "2013-06-28T00:00:00.000Z",
+            "tags": [
+                null
+            ],
+            "a": 21000000,
+            "b": 19051150,
+            "c": 19051150,
+            "d": null,
+        },
+       
+    ]
+}
+export default data;
