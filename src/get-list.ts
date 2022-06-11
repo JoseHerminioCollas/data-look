43744a6d3807b24a007fa00d1448365d60466548
Jose Collas <joseherminiocollas@goatstone.com>
2022-06-10 20:01:46 -0500

    set build for gitpages

--- src/get-list.ts
+++
@@ -1,8 +0,0 @@
-const getList = () => [...Array(10)].map((v, i) => ({
-  id: String(i),
-  name: String.fromCodePoint(Math.floor(Math.random() * 10) + 65),
-  a: 'a',
-  b: 'b',
-}));
-
-export default getList;
